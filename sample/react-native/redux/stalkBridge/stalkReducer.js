ea3fafe7fbf876c8ad12274d54fdba88e727a8c8
nattapon.r <nattapon.r@live.com>
2016-12-25 11:57:39 +0700

    move folder,

--- sample/react-native/redux/stalkBridge/stalkReducer.js
+++ sample/react-native/redux/stalkBridge/stalkReducer.js
@@ -3,13 +3,10 @@
  *
  * This is pure function for redux app.
  */
-
-import * as ChatlogsActions from "../chatlogs/chatlogsActions";
-import * as StalkBridgeActions from "../stalkBridge/stalkBridgeActions";
-import { AuthenActionsType } from "../auth/authActions";
-
-import { Record } from 'immutable';
-
+"use strict";
+const ChatlogsActions = require("../chatlogs/chatlogsActions");
+const authActions_1 = require("../auth/authActions");
+const immutable_1 = require("immutable");
 /**
  * ## Initial State
  */
@@ -18,41 +15,40 @@ import { Record } from 'immutable';
  * This Record contains the state of the form and the
  * fields it contains.
  */
-export const StalkInitState = Record({
+exports.StalkInitState = immutable_1.Record({
     isInit: false,
     chatslogComponent: null,
     chatsLog: null,
     isFetching: false,
     state: null
 });
-const initialState = new StalkInitState;
-
-export function stalkReducer(state = initialState, action) {
-    if (!(state instanceof StalkInitState)) return initialState.mergeDeep(state);
-
+const initialState = new exports.StalkInitState;
+function stalkReducer(state = initialState, action) {
+    if (!(state instanceof exports.StalkInitState))
+        return initialState.mergeDeep(state);
     switch (action.type) {
         case ChatlogsActions.STALK_INIT_CHATSLOG: {
             return state.set("isInit", true)
                 .set("chatslogComponent", action.payload);
         }
-        case ChatlogsActions.STALK_GET_CHATSLOG_COMPLETE : {
-            return state.set("chatsLog", action.payload);
+        case ChatlogsActions.STALK_GET_CHATSLOG_COMPLETE: {
+            return state.set("chatsLog", action.payload).set("state", ChatlogsActions.STALK_GET_CHATSLOG_COMPLETE);
         }
         case ChatlogsActions.STALK_CHATSLOG_CONTACT_COMPLETE: {
             let nextState = state.set("state", ChatlogsActions.STALK_CHATSLOG_CONTACT_COMPLETE)
                 .set("chatsLog", action.payload);
-            return nextState
+            return nextState;
         }
         case ChatlogsActions.STALK_UNREAD_MAP_CHANGED: {
             let nextState = state.set("chatsLog", action.payload)
                 .set("state", ChatlogsActions.STALK_UNREAD_MAP_CHANGED);
             return nextState;
         }
-
-        case AuthenActionsType.LOGOUT_SUCCESS: {
+        case authActions_1.AuthenActionsType.LOGOUT_SUCCESS: {
             return initialState;
         }
         default:
             return state;
     }
 }
+exports.stalkReducer = stalkReducer;
