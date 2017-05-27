77819a75446cfffcc04c17b2c1241573623d4201
Nattapon Rattajak <rattajak.n@gmail.com>
2017-05-27 18:28:08 +0700

    introduce browser libs.

---
+++ index.ts
@@ -0,0 +1,5 @@
+import  {ServerImplemented, IDictionary, ServerParam } from "./lib/browser/serverImplemented";
+
+export type Dict = IDictionary;
+export type Stalk = ServerImplemented;
+export type ServerParam = ServerParam;
