ba2b3848ddfc3520f51bf59d7750c308a75c71b8
Nattapon Rattajak <rattajak.n@gmail.com>
2018-01-06 22:22:06 +0700

    no message

--- starter/index.d.ts
+++ starter/index.d.ts
@@ -1,2 +1,6 @@
 export * from "./BackendFactory";
 export * from "./ServerEventListener";
+export * from "./DataListener";
+export * from "./PushDataListener";
+export * from "./simpleChat/ChatslogComponent";
+export * from "./simpleChat/InternalStore";
--- starter/index.js
+++ starter/index.js
@@ -1,2 +1,7 @@
 export * from "./BackendFactory";
 export * from "./ServerEventListener";
+export * from "./DataListener";
+export * from "./PushDataListener";
+export * from "./simpleChat/ChatslogComponent";
+export * from "./simpleChat/InternalStore";
+// export * from "./simpleChat/"; 
