50f7cfbbf924ae84abdcb3c4c7a1e921a0da2d6d
Nattapon Rattajak <rattajak.n@gmail.com>
2018-01-07 00:02:35 +0700

    add chatroom comp starter.

--- starter/simpleChat/index.d.ts
+++ starter/simpleChat/index.d.ts
@@ -1,4 +1,6 @@
 export * from "./ChatslogComponent";
+export * from "./ChatRoomComponent";
 export * from "./InternalStore";
 export * from "./utils/CryptoHelper";
 export * from "./utils/secure/SecureServiceFactory";
+export * from "./utils/secure/ISecureService";
--- starter/simpleChat/index.js
+++ starter/simpleChat/index.js
@@ -1,4 +1,5 @@
 export * from "./ChatslogComponent";
+export * from "./ChatRoomComponent";
 export * from "./InternalStore";
 export * from "./utils/CryptoHelper";
 export * from "./utils/secure/SecureServiceFactory";
